0ac5eebb21f8c8ddd09c27f158d9665bc02f1108
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-02 21:15:20 +0800

    hello api route to call trump API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    const response = await fetch(`https://www.tronalddump.io/random/quote`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/hal+json',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    res.status(200).json(data || { name: 'hello from the API YOU!!' });
+  } catch (error) {}
+};
