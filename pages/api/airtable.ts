cb6b2829da14218c815491b3cfa4f2ab2f23236e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-04 20:26:41 +0800

    test airtable endpoint

---
+++ pages/api/airtable.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    const response = await fetch(`${process.env.AIRTABLE_API_URL}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `${process.env.AIRTABLE_API_KEY}`,
+      },
+    });
+    const data = await response.json();
+    // console.log(data);
+    res.status(200).json(data || { name: 'hello from the API YOU!!' });
+  } catch (error) {}
+};
