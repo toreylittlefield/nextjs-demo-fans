5b055e9eade6644be6a77cc3e67cebb59c3a39ea
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-10 23:27:43 +0800

    twitch auth

--- pages/example.tsx
+++ pages/example.tsx
@@ -5,7 +5,7 @@ function ExampleFetch({ data }: any) {
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(`${process.env.SERVER}api/hello`);
+  const res = await fetch(`${process.env.SERVER}/api/hello`);
   const data = await res.json();
 
   // Pass data to the page via props
