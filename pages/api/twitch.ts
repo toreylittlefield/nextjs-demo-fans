d5a3db729854278e94b0b8680d5c2dc261ea23f1
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-10 23:27:51 +0800

    twitch auth

---
+++ pages/api/twitch.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    const response = await fetch(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`,
+      {
+        method: 'POST',
+      }
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {}
+};
